ea7db141361a16706c7c6b21067eac15266c9ac1
Ornette Coleman <omniosi@gmail.com>
2025-07-03 16:39:04 -0400

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'astro/config';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+
+import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
+
+const env = loadEnv(import.meta.env.MODE, process.cwd(), '');
+const { NETLIFY, STORYBLOK_DELIVERY_API_TOKEN, STORYBLOK_API_BASE_URL } = env;
+
+export default defineConfig({
+	integrations: [
+		storyblok({
+			accessToken: STORYBLOK_DELIVERY_API_TOKEN,
+			apiOptions: {
+				/** Set the correct region for your space. Learn more: https://www.storyblok.com/docs/packages/storyblok-js#example-region-parameter */
+				region: 'eu',
+				/** The following code is only required when creating a Storyblok space directly via the Blueprints feature. */
+				endpoint: STORYBLOK_API_BASE_URL
+					? `${new URL(STORYBLOK_API_BASE_URL).origin}/v2`
+					: undefined,
+			},
+			components: {
+				page: 'storyblok/Page',
+				grid: 'storyblok/Grid',
+				feature: 'storyblok/Feature',
+				teaser: 'storyblok/Teaser',
+			},
+		}),
+	],
+	output: 'server',
+	adapter: NETLIFY ? netlify() : vercel(),
+	vite: {
+		plugins: [mkcert()],
+	},
+});
